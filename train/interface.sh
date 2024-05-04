6d33fffab6b39b0c7f910128634920bac5a9fd49
TobyYang7 <tobyyang7@foxmail.com>
2024-05-04 09:11:49 +0800

    v1

--- train/interface.sh
+++ train/interface.sh
@@ -2,6 +2,6 @@ export USE_MODELSCOPE_HUB=1
 MODEL_NAME="v1"
 
 CUDA_VISIBLE_DEVICES=3 python web_demo.py \
-    --template llama3 \
+    --template qwen \
     --model_name_or_path qwen/Qwen-7B-Chat \
     --adapter_name_or_path saves/$MODEL_NAME \
